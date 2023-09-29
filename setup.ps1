b4048721d50798eb23b4d907e48d9ad41a6834ef
singsangsingsang1 <113302450+singsangsingsang1@users.noreply.github.com>
2023-09-29 12:23:33 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,5 @@
 Set-DisplayResolution -Width 1920 -Height 1080 -Force
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-& {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
-& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-
 
 $folderPath = "C:\livestream"
 $pythonScriptUrl = "https://raw.githubusercontent.com/singsangsingsang1/host/main/main.py"
