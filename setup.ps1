2f30dcba79fe3e1015ef27fb6ef84089f53cc557
singsangsingsang1 <113302450+singsangsingsang1@users.noreply.github.com>
2024-06-04 04:13:26 -0700

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,10 +3,11 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
 $folderPath = "C:\livestream"
 $pythonScriptUrl = "https://raw.githubusercontent.com/singsangsingsang1/host/main/main.py"
+$packageSetupScript = "https://raw.githubusercontent.com/singsangsingsang1/host/main/packages.py"
 $dllFileUrl = "https://github.com/singsangsingsang1/host/raw/main/LivestreamProcessor.dll"
 
 New-Item -Path $folderPath -ItemType Directory -Force
 
 Invoke-WebRequest -Uri $pythonScriptUrl -OutFile "$folderPath\main.py"
-
+Invoke-WebRequest -Uri $packageSetupScript -OutFile "$folderPath\packages.py"
 Invoke-WebRequest -Uri $dllFileUrl -OutFile "$folderPath\LivestreamProcessor.dll"
